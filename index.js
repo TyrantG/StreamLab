b56f98f79626d708fef63e856e84d1427458d3ea
tyrantgenesis <genesis489449701@gmail.com>
2022-07-12 19:04:16 +0800

    channel data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// Data Init
+const File = java.io.File
+
 const empty = 'hiker://empty'
+const LOCAL_URL = "hiker://files/rules/TyrantG/StreamLab/"
+const REAL_PATH = "/storage/emulated/0/Android/data/com.example.hikerview/files/Documents/rules/TyrantG/StreamLab/"
 const selectColor = config.selectColor
 const d = []
 
-// Constant
 const tabBar = [
   {
     id: '0',
@@ -11,22 +13,38 @@ const tabBar = [
   },
   {
     id: '1',
-    title: '关注',
+    title: '订阅',
   },
   {
     id: '2',
+    title: '频道仓库',
+  },
+  {
+    id: '3',
     title: '设置',
   },
 ]
 
-// Database
 const tabKey = getItem('StreamLab.tabKey', '0')
 const streamChannelKey = getItem('StreamLab.streamChannelKey')
 
-
-// Methods
 const makeSelectTitle = (title) => {
   return '‘‘’’<b><span style="color: '+selectColor+'">'+title+'</span></b>'
+}
+
+const getAllFilesByDir = (dir) => {
+  const dirPath = new File(REAL_PATH+dir)
+  if (dirPath.isDirectory()) {
+    const fileList = dirPath.listFiles()
+    let files = []
+    for (let index in fileList) {
+      files.push(fileList[index].getPath())
+    }
+    return files
+  } else {
+    dirPath.mkdirs()
+    return []
+  }
 }
 
 const tabBarView = () => {
@@ -46,7 +64,23 @@ const tabBarView = () => {
 const sectionView = () => {
   switch (parseInt(tabKey)) {
     case 0: {
+      const channels = getAllFilesByDir('channels')
+      if (channels.length > 0) {
 
+      } else {
+        d.push({
+          title: '还没有导入频道',
+          col_type: 'text_center_1',
+          url: $(empty).lazyRule(() => {
+            setItem('StreamLab.tabKey', '2')
+            refreshPage(true)
+            return 'hiker://empty'
+          }),
+          extra: {
+            lineVisible: false,
+          },
+        })
+      }
       break
     }
     case 1: {
@@ -54,11 +88,23 @@ const sectionView = () => {
       break
     }
     case 2: {
+      const channels = fetch("https://ghproxy.com/https://raw.githubusercontent.com/TyrantG/StreamLab/main/data/channels.json")
+      channels.forEach(channel => {
+        d.push({
+          title: channel.title,
+          desc: '0',
+          url: 'hiker://empty',
+          pic_url: channel.image,
+          col_type: 'card_pic_3',
+        })
+      })
+      break
+    }
+    case 3: {
       d.push({
-        url: 'file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/rules/TyrantG/StreamLab/settings/release/index.html',
+        url: getPath(LOCAL_URL+'views/setting.html'),
         col_type: 'x5_webview_single',
-        desc: '100%&&float',
-        // desc: '480&&list',
+        desc: '500&&list',
       })
       break
     }
--- preprocess.js
+++ preprocess.js
@@ -1,17 +1,13 @@
 const REMOTE_URL = "https://ghproxy.com/https://raw.githubusercontent.com/TyrantG/StreamLab/main/"
 const LOCAL_URL = "hiker://files/rules/TyrantG/StreamLab/"
 
-const time = (new Date()).getTime()
-
 let settingData
 if (! request(LOCAL_URL+'data/setting.json')) {
   settingData = request(LOCAL_URL+'data/setting.json')
   writeFile(LOCAL_URL+'data/setting.json', fetch(REMOTE_URL+'data/setting.json'))
 }
 
-//writeFile(LOCAL_URL+'settings/release/index.html', fetch(REMOTE_URL+'settings/release/index.html?time='+time))
-//writeFile(LOCAL_URL+'settings/release/assets/index.css', fetch(REMOTE_URL+'settings/release/assets/index.css?time='+time))
-//writeFile(LOCAL_URL+'settings/release/assets/index.js', fetch(REMOTE_URL+'settings/release/assets/index.js?time='+time))
+writeFile(LOCAL_URL+'views/setting.html', fetch(REMOTE_URL+'views/setting.html'))
 
 const setting_json = request(LOCAL_URL+'data/setting.json')
 putVar('StreamLab.config', setting_json)
